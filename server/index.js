5b0ff549f025edf667ec700d449176e1c02d88e0
Seamus <seamusugochukwu@gmail.com>
2020-11-17 13:59:24 +0100

    created two empty folders for client and server, initialized an empty react app in the client directory and install neccesary dependencies and also did the same in the server directory

---
+++ server/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import mongoose from "mongoose";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+// https://www.mongodb.com/cloud/atlas
