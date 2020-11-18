17ce750e03f944f203be68913c3c99d8517cec1f
Seamus <seamusugochukwu@gmail.com>
2020-11-18 15:00:28 +0100

    connected the server to mongodb atlas and server successfully running on designated port

--- server/index.js
+++ server/index.js
@@ -9,4 +9,15 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-// https://www.mongodb.com/cloud/atlas
+const CONNECTION_URL =
+  "mongodb+srv://base-user_96:wzPngg3DK5x8rJPP@cluster0.x6i0k.mongodb.net/<dbname>?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
+
+mongoose.set("useFindAndModify", false);
